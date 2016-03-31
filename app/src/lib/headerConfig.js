4c2c6666631b0b21d45a1827419e5b4f68d86dda
ann.golden <annmg@cox.net>
2016-03-31 09:44:57 -0400

    fixes to checkbox behavior, endpoint edit

--- app/src/lib/headerConfig.js
+++ app/src/lib/headerConfig.js
@@ -1,6 +1,6 @@
 export default {
   appServiceEndpoint: '/service/apps/1.0',
-  bsxServiceEndpoint: '/service/csx/proxy/1.1',
+  bsxServiceEndpoint: 'https://chm.363-283.io/service/csx/proxy/1.1',
   csxProxyEndpoint: 'https://chm.363-283.io/material/#/chimera-search/search',
   csxProxySearchHelpEndpoint: 'https://chm.363-283.io/material/#/help/chimerasearch',
   useLegacySearch: true,
